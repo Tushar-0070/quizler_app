c4d22b745f86b5f0a947b426b1c4818418e8e24d
azazelyash <jss.yash085@gmail.com>
2022-02-04 18:56:26 +0530

    Third Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'questions.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,11 +27,15 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scorekeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
+  
+
+  List<Question> questionList = [
+      Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+      Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
+      Question(q: 'A slug\'s blood is green.', a: true),
   ];
+
+
   int count = 0;
 
   @override
@@ -45,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[count],
+                questionList[count].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -55,6 +60,9 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
+
+        //True Button
+        
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
@@ -70,6 +78,14 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = questionList[count].questionAnswer;
+
+                if(correctAnswer == true){
+                  print('Correct!');
+                } else {
+                  print('Wrong!');
+                }
+
                 setState(() {
                   count++;
                 });
@@ -78,6 +94,9 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
+
+        //False Button
+
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
@@ -94,10 +113,25 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                bool correctAnswer = questionList[count].questionAnswer;
+                
+                if(correctAnswer == false){
+                  print('Correct!');
+                } else {
+                  print('Wrong!');
+                }
+
+                setState(() {
+                  count++;
+                });
+                print(count);
               },
             ),
           ),
         ),
+
+        //Icons Correct or Wrong
+
         Row(
           children: scorekeeper,
         )
